d0a82e812885818817b07ae943605a6666b96364
TAKMU <allan.miyazono@gmail.com>
2023-03-06 18:29:37 -0600

    Delete node_modules

--- Mobile/Views/Login.js
+++ Mobile/Views/Login.js
@@ -19,7 +19,7 @@ export default function Login( {navigation} ) {
             Username: username,
             Password: password
         };
-        fetch('http://192.168.86.21:4000/LoginMobile', {
+        fetch('http://192.168.0.177:4000/LoginMobile', {
             method: 'POST',
             body: JSON.stringify(payload),
             headers: {
