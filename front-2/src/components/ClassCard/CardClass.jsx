7e4e65749f2249d437d2e715c780bf7ce87a2a8f
Juan Ramsés Meza Martínez <104665946+imRamsesWardof@users.noreply.github.com>
2023-03-13 08:40:22 -0600

    Add StudentsHome

--- front-2/src/components/ClassCard/CardClass.jsx
+++ front-2/src/components/ClassCard/CardClass.jsx
@@ -1,20 +1,58 @@
 import React from "react";
 import "./CardClass.css";
+import Avatar from "@mui/material/Avatar";
+import { deepOrange } from "@mui/material/colors";
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+import Tooltip from '@mui/material/Tooltip';
+import { Folder, Group } from "@mui/icons-material";
+
 
 function CardClass() {
   return (
     <div className="card-class">
       <div className="card">
         <div className="card-header">
-          <h3 className="card-header__title-class">Español</h3>
-          <h4 className="card-header__teacher-name">Juan Ramses Meza Martínez</h4>
+          <Avatar sx={{ bgcolor: deepOrange[500] }}>Es</Avatar>
+          <div className="card-header__group">
+            <label className="card-header__title-class">Español</label>
+            <h4 className="card-header__teacher-name">
+              Juan Ramses Meza Martínez
+            </h4>
+          </div>
         </div>
         <div className="card-body">
-          <p className="card-header__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur officiis consequuntur optio assumenda dicta molestiae sint illo consequatur animi fugit excepturi voluptatibus nihil, non alias iste deleniti, temporibus, perspiciatis possimus?</p>
+          <p className="card-header__description">
+            ebitis. Voluptates harum odio incidunt, suscipit molestias, numquam
+            voluptatum eveniet debitis deleniti accusamus at quasi quisquam
+            minus possimus illum? Optio. Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit. Tenetur officiis consequuntur optio assumenda
+            dicta molestiae sint illo consequatur animi fugit excepturi
+            voluptatibus nihil, non alias iste deleniti, temporibus,
+            perspiciatis possimus?
+          </p>
         </div>
         <div className="card-footer">
-          <button className="btn" >
-            Unirse
+          <Stack className="card-footer__btn" direction="row" spacing={1}>
+            <Tooltip title="Estudiantes">
+              <IconButton color="primary" aria-label="add to shopping cart">
+                <Group />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Tareas">
+              <IconButton color="primary" aria-label="add an alarm">
+                <Folder />
+              </IconButton>
+            </Tooltip>
+          </Stack>
+          <button
+            className="btn"
+            onClick={() => {
+              window.location.href =
+                "http://localhost:3000/Admin/Classes/Edit/" + "Class_Id";
+            }}
+          >
+            Go
           </button>
         </div>
       </div>
--- front-2/src/views/StudentHome/StudentHome.jsx
+++ front-2/src/views/StudentHome/StudentHome.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './StudentHome.css'
 import CardClass from '../../components/ClassCard/CardClass.jsx'
+import NavBar from '../../components/NavBar/NavBar.jsx'
+
 function StudentHome() {
+
+  useEffect(() => {
+    document.title = 'Home';
+    const icon = document.getElementById("010101")
+    icon.setAttribute("href", "../src/assets/images/house-solid.svg")
+  })
+
   return (
     <div className='student-home'>
+      <NavBar/>
       <div className='student-home__cards'>
+        <CardClass/>
+        <CardClass/>
         <CardClass/>
         <CardClass/>
         <CardClass/>
