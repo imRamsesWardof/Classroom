0ac8d82f7aabaeb8e1a44d8ea76b8546bb5d403f
TAKMU <allan.miyazono@gmail.com>
2023-03-27 19:39:37 -0600

    Update Login

--- Mobile/Views/Login.js
+++ Mobile/Views/Login.js
@@ -14,6 +14,7 @@ export default function Login( {navigation} ) {
     useEffect(() => {
         console.log(auth)
     }, [auth])
+    
     const loginAPI = () => {
         const payload = {
             Username: username,
