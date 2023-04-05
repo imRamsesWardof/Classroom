9c9b7d35b59f5bb4c1921f13fa32979100176adf
TAKMU <allan.miyazono@gmail.com>
2023-04-04 18:55:28 -0600

    change basic data

--- backend/src/classroom.db.js
+++ backend/src/classroom.db.js
@@ -2,8 +2,8 @@ import {createPool} from 'mysql2/promise'
 
 export const pool = createPool({
     host : 'localhost',
-    user : 'root',
-    password : '',
+    user : 'allanmiya',
+    password : '12345',
     port : 3306,
     database: 'classroom'
 })
